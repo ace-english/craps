3ebb8882b10ca95280675f2395ecb1116885172c
Angela English <angelacaurienglish@csus.edu>
2018-03-02 17:23:02 -0800

    Added interface for brainstorming and function stubs!

---
+++ _BLACKRAIN/Craps/app/src/main/java/com/example/karan/craps/Crap_Main.java
@@ -0,0 +1,41 @@
+package com.example.karan.craps;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
+import android.widget.TextView.OnEditorActionListener;
+import android.view.inputmethod.EditorInfo;
+import android.content.Intent;
+
+public class Crap_Main extends AppCompatActivity implements OnClickListener
+{
+    /*initailizer Area for buttons, text view, booleans, edit text, etc*/
+    private Button button3;
+
+
+/*Area for creating the onclick listeners and objects for use in the program*/
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crap__main);
+
+        button3 = (Button) findViewById(R.id.button3);
+    }
+
+    @Override
+    public void onClick(View v)
+    {
+        if (v.getId()==R.id.Send)
+        {
+            if (/*code for if statement for changing to next page or creating popups. Change the variables to match next page name*/)
+            {
+                Intent intent = new Intent(this, Main2Activity.class);
+                startActivity(intent);
+            }
+            else Verify.setText("Incorrect");
+        }
+}
---
+++ _BLACKRAIN/Craps/app/src/main/java/com/example/karan/craps/Craps_Interface.java
@@ -0,0 +1,9 @@
+public interface Craps_Interface{
+	
+	/*functions stubs go here
+	 * Examples:
+	 * 
+	 * boolean foo(int argument);
+	 * int bar();
+	 */
+}
