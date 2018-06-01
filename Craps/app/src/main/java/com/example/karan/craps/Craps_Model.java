609bb9644c29be0e641265b0b9ea7306877cd7b4
kvang0200 <kvang0900@gmail.com>
2018-06-01 16:20:00 -0700

    clean up

---
+++ Craps/app/src/main/java/com/example/karan/craps/Craps_Model.java
@@ -0,0 +1,108 @@
+package com.example.karan.craps;
+import java.util.Random;
+
+public class Craps_Model implements Craps_Interface{
+	private int wallet, passLineBet, counter;
+	private boolean comeOutRoll;
+	int dice[] = new int[2];
+
+	public Craps_Model(){
+		wallet=500;
+		newGame();
+	}
+	
+	public int getWallet() {
+		return wallet;
+	}
+	
+	public int getTurnNumber() {
+		return counter;
+	}
+	
+	public boolean placeBet(int betAmount)	//returns false if insufficient funds
+	{
+		if(wallet >= betAmount)
+		{
+			wallet-=betAmount;
+			passLineBet+=betAmount;
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean newGame() // This will clear last game and create a new one.
+	{
+		counter=0;
+		passLineBet=0;
+		comeOutRoll=true;
+		//TODO: initialize text fields in interface
+		return true;
+	}
+	public boolean loseGame()   // This will excutute a Lose game if the Dice rolls didnt go the players way.
+	{
+		//TODO
+		return false;
+	}
+	public boolean winGame()	// This will excutute a Win if the Dice rolls went the players way.
+	{
+		FieldBetPayout();
+		return true;
+	}
+	public boolean FieldBetPayout()
+	{
+		wallet+=passLineBet;
+		return true;
+	}
+	
+	public int rollDice()
+	{
+		initializeDice();
+		
+		if(comeOutRoll) {
+			switch(getPointValue()) {
+				case 2:
+				case 3:
+				case 12:
+					loseGame();
+					break;
+				case 7:
+				case 11:
+					winGame();
+					break;
+				default:
+					comeOutRoll=false;
+			}
+		}
+		else {
+			counter++;
+			if(getPointValue()==7)
+				loseGame();
+			else /*if (getPointValue()==pointTextBox.Text)*/ {						// Disabled if statement for the moment for we dont have pointTextBox implemented yet.
+				//TODO: change pointTextBox to the equivalent for our interface
+				//might have to be an argument? not sure
+				winGame();
+			}
+		}
+		return dice[0]+dice[1];
+	}
+
+	private int initializeDice() {
+		Random rand= new Random();
+		
+		dice[0]=rand.nextInt(6)+1;
+		dice[1]=rand.nextInt(6)+1;
+		return dice[0] + dice[1];
+	}
+	
+	public int getPointValue() {
+		return getDie1()+getDie2();
+	}
+	
+	public int getDie1() {
+		return dice[0];
+	}
+	
+	public int getDie2() {
+		return dice[1];
+	}
+}
---
+++ Craps/app/src/main/java/com/example/karan/craps/Craps_Table.java
@@ -0,0 +1,11 @@
+package com.example.karan.craps;
+
+/**
+ * Created by Ultra-Combo on 6/1/2018.
+ */
+
+public class Craps_Table {
+
+
+
+}
